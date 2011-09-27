e3b496833f4784850537538e6dbca86cba8a8c94
Lishaak Bystroushaak <bystrousak@kitakitsune.org>
2011-09-27 05:29:13 +0200

    Added getopt option parsing and help.

--- shell2ircd.d
+++ shell2ircd.d
@@ -1,7 +1,13 @@
+/**
+ * Shell to IRC redirector. 
+*/ 
+
+
 import std.stdio;
 import std.socket;
 import std.string;
 import std.algorithm : remove;
+import std.getopt;
 
 import frozenidea2;
 
@@ -23,6 +29,11 @@ class ShellToIRC : IRCbot{
 		this.connect(server, irc_port);
 	}
 	
+	/**
+	 * Rewrited run().
+	 * 
+	 * Now, in run are handled irc and local socket. 
+	*/ 
 	override public void run(){
 		Socket[] local_connections;
 		SocketSet chk = new SocketSet();
@@ -59,9 +70,14 @@ class ShellToIRC : IRCbot{
 				local_read = local_connections[i].receive(local_buff);
 				if (local_read != 0 && local_read != Socket.ERROR){
 					string local_msg = std.conv.to!string(local_buff[0 .. local_read]);
-					// local queue :S
+					// local queue :S - ta to fixne
 					
-					this.sendMsg("#testchan", local_msg);
+					try{
+						this.sendMsg("#testchan", local_msg);
+					}catch(Exception){
+						this.join("#testchan");
+						this.sendMsg("#testchan", local_msg);
+					}
 					
 					local_buff.clear();
 				}
@@ -107,14 +123,86 @@ class ShellToIRC : IRCbot{
 			}
 		}
 	}
-	
-	public void onServerConnected(){
-		this.join("#testchan");
-	}
 }
 
-void main(){
-	ShellToIRC s = new ShellToIRC("FrozenIdea2");
-	s.connect_shell(2222, "irc.2600.net");
+
+void printHelp(string progname, ref File o = stderr){
+	o.writeln("Usage:");
+	o.writeln(
+		"\t" ~ progname ~ " [-h, -n, --nick] LOCAL_PORT IRC_SERVER IRC_PORT\n\n"
+		"\t-n, --nick\n"
+		"\t\tBot nick. Default is FrozenIdea2.\n\n"
+		"\tLOCAL_PORT\n"
+		"\t\tLocal port where daemon wait for commands.\n\n"
+		"\tIRC_SERVER\n"
+		"\t\tIRC server. SSL is not supported.\n\n"
+		"\tIRC_PORT\n"
+		"\t\tIRC port.\n\n"
+	);
+}
+
+
+
+int main(string[] args){
+	bool help;
+	string nick = "FrozenIdea2";
+	ushort local_port, irc_port;
+	string server;
+	
+	// parse options
+	try{
+		getopt(
+			args,
+			std.getopt.config.bundling,
+			"help|h", &help,
+			"nick|n", &nick
+		);
+	}catch(Exception e){
+		writeln(e.msg);
+		return -1;
+	}
+	if (help){
+		writeln("Shell to IRC redirector Daemon by Bystroushaak (bystrousak@kitakitsune.org)\n");
+		printHelp(args[0], stdout);
+		writeln(args);
+		return 0;
+	}	
+	if (args.length != 4){
+		stderr.writeln("You have to specify all position arguments!");
+		return 1;
+	}
+	try{
+		local_port = std.conv.to!ushort(args[1]);
+		
+		if (local_port < 1 || local_port > 65535)
+			throw new Exception("");
+	}catch(Exception){
+		stderr.writeln("LOCAL_PORT must be number from 1 to 65535 and not '" ~ args[1] ~ "'!");
+		return 2;
+	}
+	try{
+		irc_port = std.conv.to!ushort(args[3]);
+		
+		if (irc_port < 1 || irc_port > 65535)
+			throw new Exception("");
+	}catch(Exception){
+		stderr.writeln("IRC_PORT must be number from 1 to 65535 and not '" ~ args[3] ~ "'!");
+		return 3;
+	}
+	
+	ShellToIRC s = new ShellToIRC(nick);
+	
+	try{
+		s.connect_shell(local_port, args[2], irc_port);
+	}catch(std.socket.AddressException e){
+		stderr.writeln(e.msg);
+		return 10;
+	}catch(std.socket.SocketException e){
+		stderr.writeln(e.msg);
+		return 20;
+	}
+	
 	s.run();
+	
+	return 0;
 }
